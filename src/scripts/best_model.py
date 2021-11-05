ba01653cccbc6422b4fb66a0d0bdb777209ba50c
AndreaFalanti <a.falanti@gmail.com>
2021-11-05 23:44:07 +0100

    Improve best model training script
    Change a bit the data augmentation and the callbacks. Improve logs printed at the end of the training.

--- src/scripts/best_model.py
+++ src/scripts/best_model.py
@@ -11,6 +11,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.utils import to_categorical
 
 import log_service
+from utils.timing_callback import TimingCallback
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # disable Tensorflow info messages
 
@@ -42,24 +43,21 @@ def apply_data_augmentation():
     if use_data_augmentation:
         train_datagen = ImageDataGenerator(horizontal_flip=True,
                                            rotation_range=20,
-                                           width_shift_range=6,
-                                           height_shift_range=6,
+                                           width_shift_range=4,
+                                           height_shift_range=4,
+                                           zoom_range=0.1,
                                            rescale=1. / 255)
-        validation_datagen = ImageDataGenerator(horizontal_flip=True,
-                                                rotation_range=20,
-                                                width_shift_range=6,
-                                                height_shift_range=6,
-                                                rescale=1. / 255)
     else:
         train_datagen = ImageDataGenerator()
-        validation_datagen = ImageDataGenerator()
+
+    validation_datagen = ImageDataGenerator()
 
     return train_datagen, validation_datagen
 
 
 def generate_dataset(batch_size: int):
     (x_train_init, y_train_init), _ = load_dataset('cifar10')
-    x_train, x_val, y_train, y_val = train_test_split(x_train_init, y_train_init, train_size=0.85, shuffle=True, stratify=y_train_init)
+    x_train, x_val, y_train, y_val = train_test_split(x_train_init, y_train_init, train_size=0.9, shuffle=True, stratify=y_train_init)
 
     train_datagen, validation_datagen = apply_data_augmentation()
     train_datagen.fit(x_train)
@@ -79,7 +77,7 @@ def generate_dataset(batch_size: int):
     return train_dataset, validation_dataset, train_batches, val_batches
 
 
-def define_callbacks():
+def define_callbacks() -> 'list[callbacks.Callback]':
     '''
     Define callbacks used in model training.
 
@@ -89,12 +87,12 @@ def define_callbacks():
     # Save best weights
     ckpt_save_format = 'cp_e{epoch:02d}_vl{val_loss:.2f}_vacc{val_accuracy:.4f}.ckpt'
     ckpt_callback = callbacks.ModelCheckpoint(filepath=log_service.build_path('weights', ckpt_save_format),
-                                              save_weights_only=True, save_best_only=True, monitor='val_accuracy', mode='max')
+                                              save_weights_only=True, save_best_only=False, monitor='val_accuracy', mode='max')
     # By default shows losses and metrics for both training and validation
-    tb_callback = callbacks.TensorBoard(log_dir=log_service.build_path('tensorboard'), profile_batch=0, histogram_freq=0)
+    tb_callback = callbacks.TensorBoard(log_dir=log_service.build_path('tensorboard'), profile_batch=0, histogram_freq=1)
 
-    es_callback = callbacks.EarlyStopping(monitor='val_loss', patience=12, restore_best_weights=True, verbose=1)
-    plateau_callback = callbacks.ReduceLROnPlateau(factor=0.4, patience=5, verbose=1)
+    es_callback = callbacks.EarlyStopping(monitor='val_accuracy', patience=12, restore_best_weights=True, verbose=1, mode='max')
+    plateau_callback = callbacks.ReduceLROnPlateau(monitor='val_accuracy', factor=0.4, patience=5, verbose=1, mode='max')
 
     return [ckpt_callback, tb_callback, es_callback, plateau_callback]
 
@@ -125,13 +123,17 @@ def main():
 
     # Define callbacks
     train_callbacks = define_callbacks()
+    time_cb = TimingCallback()
+    train_callbacks.append(time_cb)
 
     hist = model.fit(x=train_dataset,
-                     epochs=2,
+                     epochs=300,
                      steps_per_epoch=train_batches,
                      validation_data=validation_dataset,
                      validation_steps=val_batches,
                      callbacks=train_callbacks)
+
+    training_time = sum(time_cb.logs)
 
     # hist.history is a dictionary of lists (each metric is a key)
     epoch_index, best_val_accuracy = max(enumerate(hist.history['val_accuracy']), key=operator.itemgetter(1))
@@ -139,17 +141,21 @@ def main():
                       hist.history['top_k_categorical_accuracy'][epoch_index]
     val_loss, top3_val = hist.history['val_loss'][epoch_index], hist.history['val_top_k_categorical_accuracy'][epoch_index]
 
-    logger.info('*' * 52)
+    logger.info('*' * 60)
     logger.info('Best epoch (val_accuracy) stats:')
-    logger.info('-' * 20 + ' Validation ' + '-' * 20)
+    logger.info('Best epoch index: %d', epoch_index + 1)
+    logger.info('Total epochs: %d', len(hist.history['loss']))
+    logger.info('Total training time (without callbacks): %0.4f seconds', training_time)
+    logger.info('-' * 24 + ' Validation ' + '-' * 24)
     logger.info('Validation accuracy: %0.4f', best_val_accuracy)
     logger.info('Validation loss: %0.4f', val_loss)
     logger.info('Validation top3 accuracy: %0.4f', top3_val)
-    logger.info('-' * 21 + ' Training ' + '-' * 21)
+    logger.info('-' * 25 + ' Training ' + '-' * 25)
     logger.info('Accuracy: %0.4f', acc)
     logger.info('Loss: %0.4f', loss)
     logger.info('Top3 accuracy: %0.4f', top3)
     logger.info('*' * 60)
+    logger.info('Total training time (without callbacks): %0.4f seconds', training_time)
 
 
 if __name__ == '__main__':
