73f7de5612ecf573bd33a2dcf742afbdb88ac8d9
AndreaFalanti <a.falanti@gmail.com>
2022-07-26 17:36:58 +0200

    Adjust evaluate network script to use test set

--- src/scripts/evaluate_network.py
+++ src/scripts/evaluate_network.py
@@ -2,16 +2,13 @@ import argparse
 import json
 import logging
 import os
-from pathlib import Path
 
-import pandas as pd
 import tensorflow as tf
-import tensorflow_addons as tfa
-from tensorflow.keras import metrics
+from tensorflow.keras import metrics, models
 
 import log_service
+from datasets.generator import generate_test_dataset
 from model import ModelGenerator
-from utils.dataset_utils import generate_tensorflow_datasets
 from utils.func_utils import parse_cell_structures
 from utils.rstr import rstr
 
@@ -22,29 +19,33 @@ tf.get_logger().setLevel(logging.ERROR)
 AUTOTUNE = tf.data.AUTOTUNE
 
 
-def get_best_cell_spec(log_folder_path: str):
-    training_results_csv_path = os.path.join(log_folder_path, 'csv', 'training_results.csv')
-    df = pd.read_csv(training_results_csv_path)
-    best_acc_row = df.loc[df['best val accuracy'].idxmax()]
+def get_model_cell_spec(log_folder_path: str):
+    with open(os.path.join(log_folder_path, 'cell_spec.txt'), 'r') as f:
+       cell_spec = f.read()
 
-    cell_spec = parse_cell_structures([best_acc_row['cell structure']])[0]
-    return cell_spec, best_acc_row['best val accuracy']
+    return parse_cell_structures([cell_spec])[0]
 
-
+# This script can be used to evaluate the final model trained on a test set.
+# It needs a saved model, which could be the one found during search or the one produced by final_training script (spec + checkpoint)
 def main():
-    # spec argument can be taken from model summary.txt, changing commas between tuples with ;
+    # if search_model flag is not specified, script will assume that the final_training script have been executed to train extensively a model
     parser = argparse.ArgumentParser(description="")
     parser.add_argument('-p', metavar='PATH', type=str, help="path to log folder", required=True)
-    parser.add_argument('-j', metavar='JSON_PATH', type=str, help='path to config json with training parameters', default=None)
-    parser.add_argument('-spec', metavar='CELL_SPECIFICATION', type=str, help="cell specification string", default=None)
-    parser.add_argument('--stem', help='add ImageNet stem to network architecture', action='store_true')
+    parser.add_argument('-j', metavar='JSON_PATH', type=str,
+                        help='path to config json with training parameters (used to instantiate dataset)', default=None)
+    parser.add_argument('--search_model', help='use best model found in search, with weights found on proxy training', action='store_true')
+    parser.add_argument('-f', metavar='MODEL_FOLDER', type=str, help='model folder name (default: best_model_training)', default='best_model_training')
     args = parser.parse_args()
 
-    model_path = os.path.join(args.p, 'best_model_training')
+    model_path = os.path.join(args.p, 'best_model') if args.search_model else os.path.join(args.p, args.f)
     log_service.set_log_path(model_path)
     logger = log_service.get_logger(__name__, 'eval.log')
 
-    custom_json_path = Path(__file__).parent / '../configs/final_training.json' if args.j is None else args.j
+    if args.j:
+        custom_json_path = args.j
+    else:
+        custom_json_path = os.path.join(args.p, 'restore', 'run.json') if args.search_model \
+            else os.path.join(model_path, 'run.json')
     logger.info('Reading configuration...')
     with open(custom_json_path, 'r') as f:
         config = json.load(f)
@@ -54,38 +55,38 @@ def main():
 
     # Load and prepare the dataset
     logger.info('Preparing datasets...')
-    dataset_folds, classes_count, image_shape, train_batches, val_batches = generate_tensorflow_datasets(config['dataset'], logger)
+    test_ds, classes_count, image_shape, test_batches = generate_test_dataset(config['dataset'], logger)
     logger.info('Datasets generated successfully')
 
     # Generate the model
-    cell_spec, _ = get_best_cell_spec(args.p) if args.spec is None else parse_cell_structures([args.spec])[0]
-    logger.info('Cell specification:')
-    for i, block in enumerate(cell_spec):
-        logger.info("Block %d: %s", i + 1, rstr(block))
+    if args.search_model:
+        model = models.load_model(os.path.join(args.p, 'best_model', 'saved_model.h5'))
+        logger.info('Model loaded successfully from H5 file')
+    else:
+        cell_spec = get_model_cell_spec(model_path)
+        logger.info('Cell specification:')
+        for i, block in enumerate(cell_spec):
+            logger.info("Block %d: %s", i + 1, rstr(block))
 
-    logger.info('Generating Keras model from cell specification...')
+        logger.info('Generating Keras model from cell specification...')
 
-    model_gen = ModelGenerator(cnn_config, arc_config, train_batches, output_classes_count=classes_count, image_shape=image_shape,
-                               data_augmentation_model=None)
-    model, _, last_cell_index = model_gen.build_model(cell_spec, add_imagenet_stem=args.stem)
+        model_gen = ModelGenerator(cnn_config, arc_config, test_batches, output_classes_count=classes_count, image_shape=image_shape,
+                                   data_augmentation_model=None)
+        model, _, last_cell_index = model_gen.build_model(cell_spec, add_imagenet_stem=False)
 
-    loss, loss_weights, optimizer, train_metrics = model_gen.define_training_hyperparams_and_metrics()
-    train_metrics.append(metrics.TopKCategoricalAccuracy(k=5))
-    train_metrics.append(tfa.metrics.F1Score(num_classes=classes_count))
+        loss, loss_weights, optimizer, train_metrics = model_gen.define_training_hyperparams_and_metrics()
+        train_metrics.append(metrics.TopKCategoricalAccuracy(k=5))
 
-    model.compile(optimizer=optimizer, loss=loss, loss_weights=loss_weights, metrics=train_metrics)
+        model.compile(optimizer=optimizer, loss=loss, loss_weights=loss_weights, metrics=train_metrics)
+        logger.info('Model generated successfully')
 
-    logger.info('Model generated successfully')
+        # model.summary(line_length=140, print_fn=logger.info)
 
-    # model.summary(line_length=140, print_fn=logger.info)
+        latest = tf.train.latest_checkpoint(os.path.join(model_path, 'weights'))
+        model.load_weights(latest)
+        logger.info('Weights loaded successfully from checkpoint')
 
-    train_dataset, validation_dataset = dataset_folds[0]
-
-    latest = tf.train.latest_checkpoint(os.path.join(model_path, 'weights'))
-    model.load_weights(latest)
-    logger.info('Weights loaded successfully')
-
-    results = model.evaluate(x=validation_dataset, return_dict=True)
+    results = model.evaluate(x=test_ds, return_dict=True)
 
     logger.info('Results: %s', rstr(results))
 
