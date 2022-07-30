ed0162c4642ca640c3b8c07bf3ffc5edb2795ed0
AndreaFalanti <a.falanti@gmail.com>
2022-07-30 22:55:17 +0200

    Change run module to generate log folder and errors as soon as possible
    This change is aimed to provide error causes in the companion web application, since it is hard to access console output and therefore the error causes where sometimes not recorded.

--- src/run.py
+++ src/run.py
@@ -39,26 +39,24 @@ def main():
         with open(log_service.build_path('restore', 'run.json'), 'r') as f:
             run_config = json.load(f)
     else:
+        log_service.initialize_log_folders(args.name)
+
         json_path = os.path.join('configs', 'run.json') if args.j is None else args.j
         with open(json_path, 'r') as f:
             run_config = json.load(f)
 
-    # DEBUG: To find out which devices your operations and tensors are assigned to
-    # tf.debugging.set_log_device_placement(True)
-
-    train_strategy = initialize_train_strategy(run_config['others']['train_strategy'])
-
-    validate_pareto_objectives(run_config['search_strategy']['pareto_objectives'])
-
-    # initialize folders after CPU/GPU check, just to avoid making folders when run is faulty
-    if args.r is None:
-        log_service.initialize_log_folders(args.name)
         # copy config (with args override) for possible run restore
         with open(log_service.build_path('restore', 'run.json'), 'w') as f:
             json.dump(run_config, f, indent=4)
 
     # Handle uncaught exception in a special log file
     sys.excepthook = log_service.make_exception_handler(log_service.create_critical_logger())
+
+    # DEBUG: To find out which devices your operations and tensors are assigned to
+    # tf.debugging.set_log_device_placement(True)
+
+    train_strategy = initialize_train_strategy(run_config['others']['train_strategy'])
+    validate_pareto_objectives(run_config['search_strategy']['pareto_objectives'])
 
     popnas = Popnas(run_config, train_strategy)
     popnas.start()
