e5563af7e8637b4341015a8f88a5a286953bc6a0
AndreaFalanti <a.falanti@gmail.com>
2022-08-12 17:30:49 +0200

    Add support for custom validation and test splits in time series datasets

--- src/dataset/generators/time_series_classification.py
+++ src/dataset/generators/time_series_classification.py
@@ -6,40 +6,54 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 from sktime import datasets as sktdata
 
-from dataset.generators.base import BaseDatasetGenerator, AutoShardPolicy
+from dataset.generators.base import BaseDatasetGenerator
 from dataset.preprocessing import TimeSeriesPreprocessor
+
+
+def _load_npz(file_path: str) -> 'tuple[np.ndarray, np.ndarray]':
+    npz = np.load(file_path)
+    return npz['x'], npz['y']
+
+
+def _load_ts(file_path: str) -> 'tuple[np.ndarray, np.ndarray]':
+    ''' Load .ts file into numpy array, modifying the format for TF operators. '''
+    x_train, y_train = sktdata.load_from_tsfile(file_path, return_data_type='numpy3d')
+    # ts use array format (num_series, ts_length), instead of TF 1D ops required format (ts_length, num_series). Swap the axis.
+    x_train = np.swapaxes(x_train, -2, -1)
+    # ts encode labels as str (e.g. 1.0, 2.0). Cast to int for one-hot encoding (need intermediate conversion to float).
+    y_train = y_train.astype(np.float).astype(np.int32)
+
+    return x_train, y_train
 
 
 class TimeSeriesClassificationDatasetGenerator(BaseDatasetGenerator):
     def __init__(self, dataset_config: dict):
         super().__init__(dataset_config)
 
+    def _get_numpy_split(self, split_name: str):
+        # numpy case
+        if os.path.exists(os.path.join(self.dataset_path, f'{split_name}.npz')):
+            x_train, y_train = _load_npz(os.path.join(self.dataset_path, f'{split_name}.npz'))
+        # ts (sktime) case
+        elif os.path.exists(os.path.join(self.dataset_path, f'{split_name}.ts')):
+            x_train, y_train = _load_ts(os.path.join(self.dataset_path, f'{split_name}.ts'))
+        else:
+            raise ValueError('No supported dataset format recognized at path provided in configuration')
+
+        # make a plain list to perform one_hot correctly in preprocessor
+        y_train = np.squeeze(y_train)
+
+        return x_train, y_train
+
     def generate_train_val_datasets(self) -> 'tuple[list[tuple[tf.data.Dataset, tf.data.Dataset]], int, tuple[int, ...], int, int]':
         dataset_folds = []  # type: list[tuple[tf.data.Dataset, tf.data.Dataset]]
 
         for i in range(self.dataset_folds_count):
             self._logger.info('Preprocessing and building dataset fold #%d...', i + 1)
-            shard_policy = AutoShardPolicy.DATA
 
-            # Custom dataset, loaded from numpy arrays
+            # Custom datasets
             if self.dataset_path is not None:
-                # numpy case
-                if os.path.exists(os.path.join(self.dataset_path, 'numpy_training', 'train.npz')):
-                    train_npz = np.load(os.path.join(self.dataset_path, 'numpy_training', 'train.npz'))
-                    x_train, y_train = train_npz['x'], train_npz['y']
-                # ts (sktime) case
-                elif os.path.exists(os.path.join(self.dataset_path, 'train.ts')):
-                    x_train, y_train = sktdata.load_from_tsfile(os.path.join(self.dataset_path, 'train.ts'), return_data_type='numpy3d')
-                    # don't know why, but seems they prefer (num_series, ts_length) as format instead of CONV1D required (ts_length, num_series)
-                    # swap the axis
-                    x_train = np.swapaxes(x_train, -2, -1)
-                    # cast to int, in case str is used (need intermediate conversion to float)
-                    y_train = y_train.astype(np.float).astype(np.int32)
-                else:
-                    raise ValueError('No supported dataset format recognized at path provided in configuration')
-
-                # make a plain list to perform one_hot correctly in preprocessor
-                y_train = np.squeeze(y_train)
+                x_train, y_train = self._get_numpy_split('train')
 
                 classes = len(np.unique(y_train))
                 # discard first dimension since is the number of samples
@@ -49,8 +63,11 @@ class TimeSeriesClassificationDatasetGenerator(BaseDatasetGenerator):
                     # also shuffle, since .ts files are usually ordered by class. Without shuffle, entire classes could be dropped.
                     x_train, y_train = shuffle(x_train, y_train, n_samples=self.samples_limit)
 
-                # create a validation set for evaluation of the child models
-                x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=self.val_size, stratify=y_train)
+                # create a validation set for evaluation of the child models. If val_size is None, then files for validation split must exist.
+                if self.val_size is None:
+                    x_val, y_val = self._get_numpy_split('validation')
+                else:
+                    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=self.val_size, stratify=y_train)
 
                 train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
                 val_ds = tf.data.Dataset.from_tensor_slices((x_val, y_val))
@@ -58,8 +75,8 @@ class TimeSeriesClassificationDatasetGenerator(BaseDatasetGenerator):
                 raise NotImplementedError()
 
             preprocessor = TimeSeriesPreprocessor(to_one_hot=classes)
-            train_ds, train_batches = self._finalize_dataset(train_ds, self.batch_size, preprocessor, None, shard_policy=shard_policy)
-            val_ds, val_batches = self._finalize_dataset(val_ds, self.batch_size, preprocessor, None, shard_policy=shard_policy)
+            train_ds, train_batches = self._finalize_dataset(train_ds, self.batch_size, preprocessor, None)
+            val_ds, val_batches = self._finalize_dataset(val_ds, self.batch_size, preprocessor, None)
             dataset_folds.append((train_ds, val_ds))
 
         self._logger.info('Dataset folds built successfully')
@@ -68,9 +85,20 @@ class TimeSeriesClassificationDatasetGenerator(BaseDatasetGenerator):
         return dataset_folds, classes, input_shape, train_batches, val_batches
 
     def generate_test_dataset(self) -> 'tuple[tf.data.Dataset, int, tuple, int]':
-        shard_policy = AutoShardPolicy.DATA
+        # Custom datasets
+        if self.dataset_path is not None:
+            x_test, y_test = self._get_numpy_split('test')
 
-        raise NotImplementedError('Test set is not supported yet')
+            classes = len(np.unique(y_test))
+            # discard first dimension since is the number of samples
+            input_shape = np.shape(x_test)[1:]
 
-        # self._logger.info('Test dataset built successfully')
-        # return test_ds, classes, image_shape, batches
+            test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+        else:
+            raise NotImplementedError()
+
+        preprocessor = TimeSeriesPreprocessor(to_one_hot=classes)
+        test_ds, batches = self._finalize_dataset(test_ds, self.batch_size, preprocessor, None)
+
+        self._logger.info('Test dataset built successfully')
+        return test_ds, classes, input_shape, batches
