cf31adb62ed3b4eab31a81141fb6d4a3cfb52cc9
AndreaFalanti <a.falanti@gmail.com>
2022-08-20 16:55:19 +0200

    Simplify and increase performance of CvT
    GELU approximation and various refactoring from custom implementation to new Keras layers counterparts have drastically improved the computation time. Also, it should now be a bit more consistent with original implementation (LayerNormalization maybe was not implemented correctly).

--- src/models/operators/cvt.py
+++ src/models/operators/cvt.py
@@ -6,30 +6,15 @@ import tensorflow as tf
 from einops import rearrange
 from tensorflow import einsum
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Layer, Conv2D, BatchNormalization, Dropout, Softmax
-
-
-class LayerNorm(Layer):  # layernorm, but done in the channel dimension #1
-    def __init__(self, dim, eps=1e-5):
-        super().__init__()
-        self.eps = eps
-
-        self.g = tf.Variable(tf.ones([1, 1, 1, dim]))
-        self.b = tf.Variable(tf.zeros([1, 1, 1, dim]))
-
-    def call(self, x, training=None, **kwargs):
-        var = tf.math.reduce_variance(x, axis=-1, keepdims=True)
-        mean = tf.reduce_mean(x, axis=-1, keepdims=True)
-
-        x = (x - mean) / tf.sqrt((var + self.eps)) * self.g + self.b
-        return x
+from tensorflow.keras.activations import gelu
+from tensorflow.keras.layers import Layer, Conv2D, DepthwiseConv2D, BatchNormalization, Dropout, Softmax, LayerNormalization
 
 
 class PreNorm(Layer):
-    def __init__(self, dim, fn):
+    def __init__(self, fn):
         super().__init__()
 
-        self.norm = LayerNorm(dim)
+        self.norm = LayerNormalization(epsilon=1e-5)
         self.fn = fn
 
     def call(self, x, training=None, **kwargs):
@@ -40,31 +25,36 @@ class MLP(Layer):
     def __init__(self, dim, mult=4, dropout=0.0):
         super().__init__()
 
-        self.net = [
-            Conv2D(filters=dim * mult, kernel_size=1, strides=1, activation='gelu'),
-            Dropout(rate=dropout),
-            Conv2D(filters=dim, kernel_size=1, strides=1),
-            Dropout(rate=dropout)
-        ]
-        self.net = Sequential(self.net)
+        # TODO: the real CvT implementation seems to use dense instead of pointwise conv in MLP.
+        #  But a reshape should follow, investigate.
+        self.conv_1 = Conv2D(filters=dim * mult, kernel_size=1, strides=1)
+        self.drop_1 = Dropout(rate=dropout)
+        self.conv_2 = Conv2D(filters=dim, kernel_size=1, strides=1)
+        self.drop_2 = Dropout(rate=dropout)
 
     def call(self, x, training=None, **kwargs):
-        return self.net(x, training=training)
+        x = self.conv_1(x, training=training)
+        x = gelu(x, approximate=True)
+        x = self.drop_1(x, training=training)
+        x = self.conv_2(x, training=training)
+        return self.drop_2(x, training=training)
 
 
-class DepthWiseConv2d(Layer):
-    def __init__(self, dim_in, dim_out, kernel_size, stride, bias=True):
+class CustomSeparableConv2D(Layer):
+    '''
+    Basically the usual Depthwise Separable convolution implemented also in Keras, but with BatchNormalization in between.
+    '''
+    def __init__(self, dim_out, kernel_size, stride, bias=False):
         super().__init__()
 
-        self.net = Sequential([
-            Conv2D(filters=dim_in, kernel_size=kernel_size, strides=stride, padding='SAME', groups=dim_in, use_bias=bias),
-            BatchNormalization(momentum=0.9, epsilon=1e-5),
-            Conv2D(filters=dim_out, kernel_size=1, strides=1, use_bias=bias)
-        ])
+        self.depthwise_conv = DepthwiseConv2D(kernel_size=kernel_size, strides=stride, padding='same', use_bias=bias)
+        self.b_norm = BatchNormalization(momentum=0.9, epsilon=1e-5)
+        self.pointwise_conv = Conv2D(filters=dim_out, kernel_size=1, strides=1, use_bias=bias)
 
     def call(self, x, training=None, **kwargs):
-        x = self.net(x, training=training)
-        return x
+        x = self.depthwise_conv(x, training=training)
+        x = self.b_norm(x, training=training)
+        return self.pointwise_conv(x, training=training)
 
 
 class MultiHeadSelfAttention(Layer):
@@ -76,25 +66,28 @@ class MultiHeadSelfAttention(Layer):
 
         self.attend = Softmax()
 
-        self.to_q = DepthWiseConv2d(dim, inner_dim, proj_kernel, stride=1, bias=False)
-        self.to_kv = DepthWiseConv2d(dim, inner_dim * 2, proj_kernel, stride=kv_proj_stride, bias=False)
+        self.to_q = CustomSeparableConv2D(inner_dim, proj_kernel, stride=1, bias=False)
+        self.to_kv = CustomSeparableConv2D(inner_dim * 2, proj_kernel, stride=kv_proj_stride, bias=False)
 
         self.to_out = Sequential([
+            # TODO: also this is a dense layer in official implementation.
             Conv2D(filters=dim, kernel_size=1, strides=1),
             Dropout(rate=dropout)
         ])
 
     def call(self, x, training=None, **kwargs):
-        b, _, y, n = x.shape
+        # y is needed for rearrange operations
+        _, _, y, _ = x.shape
         h = self.heads
         q = self.to_q(x, training=training)
         kv = self.to_kv(x, training=training)
         k, v = tf.split(kv, num_or_size_splits=2, axis=-1)
         qkv = (q, k, v)
+        # h is the number of heads
         q, k, v = map(lambda t: rearrange(t, 'b x y (h d) -> (b h) (x y) d', h=h), qkv)
 
-        dots = einsum('b i d, b j d -> b i j', q, k) * self.scale
-        attn = self.attend(dots)
+        attention_scores = einsum('b i d, b j d -> b i j', q, k) * self.scale
+        attn = self.attend(attention_scores)
 
         x = einsum('b i j, b j d -> b i d', attn, v)
         x = rearrange(x, '(b h) (x y) d -> b x y (h d)', h=h, y=y)
@@ -110,8 +103,8 @@ class ConvolutionalTransformerBlock(Layer):
         self.layers = []
         for _ in range(num_blocks):
             self.layers.append([
-                PreNorm(filters, MultiHeadSelfAttention(filters, proj_kernel=proj_kernel, kv_proj_stride=kv_proj_stride, heads=heads, dim_head=dim_head, dropout=dropout)),
-                PreNorm(filters, MLP(filters, mlp_mult, dropout=dropout))
+                PreNorm(MultiHeadSelfAttention(filters, proj_kernel=proj_kernel, kv_proj_stride=kv_proj_stride, heads=heads, dim_head=dim_head, dropout=dropout)),
+                PreNorm(MLP(filters, mlp_mult, dropout=dropout))
             ])
 
     def call(self, x, training=None, **kwargs):
@@ -134,7 +127,7 @@ class CVTStage(Layer):
         super().__init__(name=name)
 
         self.conv_token_embedding = Conv2D(filters=emb_dim, kernel_size=emb_kernel, padding='same', strides=emb_stride)
-        self.layer_norm = LayerNorm(emb_dim)
+        self.layer_norm = LayerNormalization(epsilon=1e-5)
         self.conv_transformer_block = ConvolutionalTransformerBlock(filters=emb_dim, proj_kernel=proj_kernel,
                                                                     kv_proj_stride=kv_proj_stride, num_blocks=ct_blocks, heads=heads,
                                                                     mlp_mult=mlp_mult, dropout=dropout)
