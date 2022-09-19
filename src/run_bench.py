47175ccc742a5b4310f3b56a4ea409ddb122c256
AndreaFalanti <a.falanti@gmail.com>
2022-09-19 21:06:58 +0200

    Automatize NATS-bench trials on all supported datasets
    run_bench.py script now execute 3 runs on each dataset supported by NATS-bench, producing results for comparison with other methods (see table 4 of NATS-bench paper). Evaluate script has been removed since it is now integrated in run_bench.

--- src/run_bench.py
+++ src/run_bench.py
@@ -1,11 +1,18 @@
 import argparse
 import json
+import os.path
 import sys
 
+import numpy as np
+import pandas as pd
+
 import log_service
+from benchmarking import NATSbench
 from popnas import Popnas
 from utils.config_validator import validate_config_json
+from utils.func_utils import create_empty_folder, parse_cell_structures
 from utils.nn_utils import initialize_train_strategy
+from utils.rstr import rstr
 
 
 def generate_popnas_bench_config(dataset_name: str, bench_path: str):
@@ -85,34 +92,80 @@ def generate_popnas_bench_config(dataset_name: str, bench_path: str):
     }
 
 
+def get_best_cell_spec(log_folder_path: str, metric: str = 'best val accuracy'):
+    training_results_csv_path = os.path.join(log_folder_path, 'csv', 'training_results.csv')
+    df = pd.read_csv(training_results_csv_path)
+    best_acc_row = df.loc[df[metric].idxmax()]
+
+    cell_spec = parse_cell_structures([best_acc_row['cell structure']])[0]
+    return cell_spec, best_acc_row[metric]
+
+
+def evaluate_best_cell_on_test(config: dict, run_folder: str):
+    # Create the API instance for the topology search space in NATS
+    bench = NATSbench(config['dataset']['path'])
+
+    best_cell_spec, best_val = get_best_cell_spec(run_folder)
+    dataset = config['dataset']['name']
+
+    test_acc = bench.simulate_testing_on_nas_bench_201(best_cell_spec, dataset)
+
+    with open(os.path.join(run_folder, 'bench-test.txt'), 'w') as f:
+        f.write(f'Cell: {rstr(best_cell_spec)}\n')
+        f.write(f'val: {best_val}, test: {test_acc}')
+
+    return best_val, test_acc
+
+
 def main():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-d', metavar='DATASET', type=str, help='name of NAS-Bench-201 dataset', default=None, required=True)
     parser.add_argument('-p', metavar='BENCH_PATH', type=str, help='path to NAS-Bench-201 files', default=None, required=True)
+    parser.add_argument('-n', metavar='N_RUNS', type=int, help='number of runs to execute on each dataset', default=3)
     parser.add_argument('--name', metavar='RUN_NAME', type=str, help='name used for log folder', default=None)
     args = parser.parse_args()
 
-    log_service.initialize_log_folders(args.name)
+    # will contain all runs executed
+    main_folder = args.name if args.name is not None else 'nas-bench-201'
+    create_empty_folder(os.path.join('logs', main_folder))
 
-    run_config = generate_popnas_bench_config(args.d, args.p)
+    supported_datasets = ('cifar10', 'cifar100', 'ImageNet16-120')
+    for dataset in supported_datasets:
+        run_config = generate_popnas_bench_config(dataset, args.p)
+        # check that the config is correct
+        validate_config_json(run_config)
 
-    # copy config (with args override) for possible run restore
-    with open(log_service.build_path('restore', 'run.json'), 'w') as f:
-        json.dump(run_config, f, indent=4)
+        train_strategy = initialize_train_strategy(run_config['others']['train_strategy'])
 
-    # Handle uncaught exception in a special log file
-    sys.excepthook = log_service.make_exception_handler(log_service.create_critical_logger())
+        val_accuracies, test_accuracies = [], []
 
-    # check that the config is correct
-    validate_config_json(run_config)
+        for i in range(args.n):
+            run_folder_path = os.path.join(main_folder, f'{dataset}_{i}')
+            log_service.initialize_log_folders(run_folder_path)
 
-    # DEBUG: To find out which devices your operations and tensors are assigned to
-    # tf.debugging.set_log_device_placement(True)
+            # copy config (with args override) for possible run restore
+            with open(log_service.build_path('restore', 'run.json'), 'w') as f:
+                json.dump(run_config, f, indent=4)
 
-    train_strategy = initialize_train_strategy(run_config['others']['train_strategy'])
+            # Handle uncaught exception in a special log file
+            sys.excepthook = log_service.make_exception_handler(log_service.create_critical_logger())
 
-    popnas = Popnas(run_config, train_strategy, benchmarking=True)
-    popnas.start()
+            # DEBUG: To find out which devices your operations and tensors are assigned to
+            # tf.debugging.set_log_device_placement(True)
+
+            popnas = Popnas(run_config, train_strategy, benchmarking=True)
+            popnas.start()
+
+            # perform final evaluation on best cell found
+            val_acc, test_acc = evaluate_best_cell_on_test(run_config, os.path.join('logs', run_folder_path))
+            val_accuracies.append(val_acc * 100)
+            test_accuracies.append(test_acc * 100)
+
+        val_mean, test_mean = np.mean(val_accuracies), np.mean(test_accuracies)
+        val_variance, test_variance = np.var(val_accuracies), np.var(val_accuracies)
+        with open(os.path.join('logs', main_folder, f'{dataset}.txt'), 'w') as f:
+            f.write(f'DATASET: {dataset}\n')
+            f.write(f'Validation accuracies: {val_accuracies} -> {val_mean:0.2f}+-({val_variance:0.2f})\n')
+            f.write(f'Test accuracies: {test_accuracies} -> {test_mean:0.2f}+-({test_variance:0.2f})\n')
 
 
 if __name__ == '__main__':
