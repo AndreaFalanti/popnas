6dc7bf73109e1b75ea44e245b90b0c3f6021635b
AndreaFalanti <a.falanti@gmail.com>
2022-09-28 22:19:40 +0200

    Add script for training the top-k networks from search procedure
    top_k_final_training.py shares a good chunk of code with final_training.py, but is designed for multiple consecutive trainings of the most promising architectures. Shared functions are placed in an external utils file, to reduce duplication.

--- src/scripts/top_k_final_training.py
+++ src/scripts/top_k_final_training.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras import callbacks, models, metrics
+from tensorflow.keras import callbacks, metrics
 from tensorflow.python.keras.utils.vis_utils import plot_model
 
 import log_service
@@ -14,11 +14,17 @@ from dataset.augmentation import get_image_data_augmentation_model
 from dataset.utils import dataset_generator_factory, generate_balanced_weights_for_classes
 from models.model_generator import ModelGenerator
 from utils.feature_utils import metrics_fields_dict
-from utils.final_training_utils import create_model_log_folder, save_trimmed_json_config, log_best_cell_results_during_search, define_callbacks, \
-    log_final_training_results
-from utils.func_utils import parse_cell_structures, cell_spec_to_str
+from utils.final_training_utils import log_best_cell_results_during_search, define_callbacks, log_final_training_results, \
+    save_trimmed_json_config, create_model_log_folder
+from utils.func_utils import create_empty_folder, parse_cell_structures, cell_spec_to_str
 from utils.nn_utils import initialize_train_strategy, get_optimized_steps_per_execution, save_keras_model_to_onnx
 from utils.timing_callback import TimingCallback
+
+# TODO: this script is similar to final_training.py, but trains multiple models (the top-k which reached the best results on score metric
+#  during the search procedure), instead of a single one.
+#  They could be merged together, but it is quite a mess already because of the multiple choices and parameters...
+#  For now they share functions through an external module, plus some duplication on the initialization part, but are more readable in this way.
+#  Still, duplication is a problem, make sure to avoid it in case of modification in the first part of the script.
 
 # disable Tensorflow info and warning messages
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # disable Tensorflow info messages
@@ -27,13 +33,13 @@ tf.get_logger().setLevel(logging.ERROR)
 AUTOTUNE = tf.data.AUTOTUNE
 
 
-def get_best_cell_spec(log_folder_path: str, metric: str = 'best val accuracy'):
+def get_best_cell_specs(log_folder_path: str, n: int, metric: str = 'best val accuracy'):
     training_results_csv_path = os.path.join(log_folder_path, 'csv', 'training_results.csv')
     df = pd.read_csv(training_results_csv_path)
-    best_acc_row = df.loc[df[metric].idxmax()]
+    best_acc_rows = df.nlargest(n, columns=[metric])
 
-    cell_spec = parse_cell_structures([best_acc_row['cell structure']])[0]
-    return cell_spec, best_acc_row[metric]
+    cell_specs = parse_cell_structures(best_acc_rows['cell structure'])
+    return zip(cell_specs, best_acc_rows[metric])
 
 
 def main():
@@ -41,12 +47,10 @@ def main():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument('-p', metavar='PATH', type=str, help="path to log folder", required=True)
     parser.add_argument('-j', metavar='JSON_PATH', type=str, help='path to config json with training parameters', default=None)
+    parser.add_argument('-n', metavar='NUM_MODELS', type=str, help='number of top models to train, when -spec or --load are not specified', default=5)
     parser.add_argument('-ts', metavar='TRAIN_STRATEGY', type=str, help='device used in Tensorflow distribute strategy', default=None)
-    parser.add_argument('-spec', metavar='CELL_SPECIFICATION', type=str, help="cell specification string", default=None)
     parser.add_argument('-name', metavar='OUTPUT_NAME', type=str, help="output location in log folder", default='best_model_training')
-    parser.add_argument('--load', help='load model from checkpoint', action='store_true')
     parser.add_argument('--same', help='use same hyperparams of the ones used during search algorithm', action='store_true')
-    parser.add_argument('--debug', help='produce debug files of the whole training procedure', action='store_true')
     parser.add_argument('--stem', help='add ImageNet stem to network architecture', action='store_true')
     args = parser.parse_args()
 
@@ -55,23 +59,16 @@ def main():
 
     custom_json_path = Path(__file__).parent / '../configs/final_training.json' if args.j is None else args.j
 
-    save_path = os.path.join(args.p, args.name)
-    create_model_log_folder(save_path)
+    save_path_prefix = os.path.join(args.p, args.name)
+
+    create_empty_folder(save_path_prefix)
+    log_service.set_log_path(save_path_prefix)
     logger = log_service.get_logger(__name__)
-
-    # NOTE: it's bugged on windows, see https://github.com/tensorflow/tensorflow/issues/43608. Run debug only on linux.
-    if args.debug:
-        tf.debugging.experimental.enable_dump_debug_info(
-            os.path.join(save_path, 'debug'),
-            tensor_debug_mode="FULL_HEALTH",
-            circular_buffer_size=-1)
-
-    with open(os.path.join(args.p, 'restore', 'run.json'), 'r') as f:
-        run_config = json.load(f)  # type: dict
 
     logger.info('Reading configuration...')
     if args.same:
-        config = run_config
+        with open(os.path.join(args.p, 'restore', 'run.json'), 'r') as f:
+            config = json.load(f)  # type: dict
     else:
         with open(custom_json_path, 'r') as f:
             config = json.load(f)   # type: dict
@@ -96,7 +93,7 @@ def main():
     # dump the json into save folder, so that is possible to retrieve how the model had been trained
     # update and prune JSON config first (especially when coming from --same flag since it has all params of search algorithm)
     cnn_config['epochs'] = epochs
-    save_trimmed_json_config(config, save_path)
+    save_trimmed_json_config(config, save_path_prefix)
     score_metric = config['search_strategy'].get('score_metric', 'accuracy')
 
     # Load and prepare the dataset
@@ -110,32 +107,34 @@ def main():
         if balance_class_losses else None
     logger.info('Datasets generated successfully')
 
-    # TODO: load model from checkpoint is more of a legacy feature right now. Delete it?
-    if args.load:
-        logger.info('Loading best model from provided folder...')
-        with train_strategy.scope():
-            model = models.load_model(os.path.join(args.p, 'best_model', 'tf_model'))  # type: models.Model
+    # TODO: maybe strategy scope is not necessary, but put just in case...
+    # Initialize model generator
+    with train_strategy.scope():
+        model_gen = ModelGenerator(cnn_config, arc_config, train_batches, output_classes_count=classes_count, input_shape=input_shape,
+                                   data_augmentation_model=get_image_data_augmentation_model() if augment_on_gpu else None)
 
-        last_cell_index = 7     # TODO
-        logger.info('Model loaded successfully')
-    else:
-        if args.spec is None:
-            logger.info('Getting best cell specification found during POPNAS run...')
-            # find best model found during search and log some relevant info
-            metric = metrics_fields_dict[score_metric].real_column
-            cell_spec, best_score = get_best_cell_spec(args.p, metric)
-            log_best_cell_results_during_search(logger, cell_spec, best_score, metric)
-        else:
-            cell_spec = parse_cell_structures([args.spec])[0]
+    logger.info('Getting best cell specification found during POPNAS run...')
+    # find best model found during search and log some relevant info
+    metric = metrics_fields_dict[score_metric].real_column
+    cell_score_iter = get_best_cell_specs(args.p, args.n, metric)
+    logger.info('Starting training procedure for each model...')
 
-        logger.info('Generating Keras model from cell specification...')
+    for i, (cell_spec, best_score) in enumerate(cell_score_iter):
+        model_folder = os.path.join(save_path_prefix, str(i))
+        create_model_log_folder(model_folder)
+        model_logger = log_service.get_logger(__name__)
+        
+        log_best_cell_results_during_search(model_logger, cell_spec, best_score, metric)
+
+        logger.info('Executing model %d training', i)
+        model_logger.info('Generating Keras model from cell specification...')
         # reconvert cell to str so that can be stored together with results (usable by other script and easier to remember what cell has been trained)
-        with open(os.path.join(save_path, 'cell_spec.txt'), 'w') as f:
+        with open(os.path.join(model_folder, 'cell_spec.txt'), 'w') as f:
             f.write(cell_spec_to_str(cell_spec))
 
+        save_trimmed_json_config(config, model_folder)
+
         with train_strategy.scope():
-            model_gen = ModelGenerator(cnn_config, arc_config, train_batches, output_classes_count=classes_count, input_shape=input_shape,
-                                       data_augmentation_model=get_image_data_augmentation_model() if augment_on_gpu else None)
             model, _, last_cell_index = model_gen.build_model(cell_spec, add_imagenet_stem=args.stem)
 
             loss, loss_weights, optimizer, train_metrics = model_gen.define_training_hyperparams_and_metrics()
@@ -144,36 +143,39 @@ def main():
             execution_steps = get_optimized_steps_per_execution(train_strategy)
             model.compile(optimizer=optimizer, loss=loss, loss_weights=loss_weights, metrics=train_metrics, steps_per_execution=execution_steps)
 
-        logger.info('Model generated successfully')
+        model_logger.info('Model generated successfully')
+    
+        model.summary(line_length=140, print_fn=model_logger.info)
+    
+        model_logger.info('Converting untrained model to ONNX')
+        save_keras_model_to_onnx(model, save_path=os.path.join(model_folder, 'untrained.onnx'))
+    
+        # TODO: right now only the first fold is used, expand the logic later to support multiple folds
+        train_dataset, validation_dataset = dataset_folds[0]
+    
+        # Define callbacks
+        train_callbacks = define_callbacks(cdr_enabled, score_metric, multi_output, last_cell_index)
+        time_cb = TimingCallback()
+        train_callbacks.append(time_cb)
+    
+        plot_model(model, to_file=os.path.join(model_folder, 'model.pdf'), show_shapes=True, show_layer_names=True)
+    
+        hist = model.fit(x=train_dataset,
+                         epochs=epochs,
+                         steps_per_epoch=train_batches,
+                         validation_data=validation_dataset,
+                         validation_steps=val_batches,
+                         class_weight=balanced_class_weights[0] if balance_class_losses else None,
+                         callbacks=train_callbacks)     # type: callbacks.History
+    
+        training_time = sum(time_cb.logs)
+        log_final_training_results(model_logger, hist, score_metric, training_time, arc_config['multi_output'])
+    
+        model_logger.info('Converting trained model to ONNX')
+        save_keras_model_to_onnx(model, save_path=os.path.join(model_folder, 'trained.onnx'))
+        logger.info('Model %d training complete', i)
 
-    model.summary(line_length=140, print_fn=logger.info)
-
-    logger.info('Converting untrained model to ONNX')
-    save_keras_model_to_onnx(model, save_path=os.path.join(save_path, 'untrained.onnx'))
-
-    # TODO: right now only the first fold is used, expand the logic later to support multiple folds
-    train_dataset, validation_dataset = dataset_folds[0]
-
-    # Define callbacks
-    train_callbacks = define_callbacks(cdr_enabled, score_metric, multi_output, last_cell_index)
-    time_cb = TimingCallback()
-    train_callbacks.append(time_cb)
-
-    plot_model(model, to_file=os.path.join(save_path, 'model.pdf'), show_shapes=True, show_layer_names=True)
-
-    hist = model.fit(x=train_dataset,
-                     epochs=epochs,
-                     steps_per_epoch=train_batches,
-                     validation_data=validation_dataset,
-                     validation_steps=val_batches,
-                     class_weight=balanced_class_weights[0] if balance_class_losses else None,
-                     callbacks=train_callbacks)     # type: callbacks.History
-
-    training_time = sum(time_cb.logs)
-    log_final_training_results(logger, hist, score_metric, training_time, arc_config['multi_output'])
-
-    logger.info('Converting trained model to ONNX')
-    save_keras_model_to_onnx(model, save_path=os.path.join(save_path, 'trained.onnx'))
+        tf.keras.backend.clear_session()
 
 
 if __name__ == '__main__':
---
+++ src/utils/final_training_utils.py
@@ -0,0 +1,115 @@
+'''
+Stores common function used in training scripts, which could be executed after the main search procedure.
+All the functions inside this file are not used inside the search algorithm.
+'''
+
+import json
+import logging
+import operator
+import os
+
+from tensorflow.keras import callbacks
+
+import log_service
+from utils.func_utils import create_empty_folder
+from utils.nn_utils import get_multi_output_best_epoch_stats
+from utils.rstr import rstr
+
+
+def define_callbacks(cdr_enabled: bool, score_metric: str, multi_output: bool, last_cell_index: int) -> 'list[callbacks.Callback]':
+    '''
+    Define callbacks used in model training.
+
+    Returns:
+        (tf.keras.Callback[]): Keras callbacks
+    '''
+    # Save best weights
+    target_metric = f'val_Softmax_c{last_cell_index}_{score_metric}' if multi_output else f'val_{score_metric}'
+    ckpt_save_format = 'cp_e{epoch:02d}_vl{val_loss:.2f}_v' + score_metric[:2] + '{' + target_metric + ':.4f}.ckpt'
+    ckpt_callback = callbacks.ModelCheckpoint(filepath=log_service.build_path('weights', ckpt_save_format),
+                                              save_weights_only=True, save_best_only=True, monitor=target_metric, mode='max')
+    # By default shows losses and metrics for both training and validation
+    tb_callback = callbacks.TensorBoard(log_dir=log_service.build_path('tensorboard'), profile_batch=0, histogram_freq=0)
+
+    # these callbacks are shared between all models
+    train_callbacks = [ckpt_callback, tb_callback]
+
+    # if using plain lr, adapt it with reduce learning rate on plateau
+    # NOTE: for unknown reasons, activating plateau callback when cdr is present will also cause an error at the end of the first epoch
+    # TODO: right now, if cdr is disabled the lr is scheduled with cosine decay, so reduceLROnPlateau could be not optimal.
+    # if not cdr_enabled:
+    #     train_callbacks.append(callbacks.ReduceLROnPlateau(monitor='val_accuracy', factor=0.4, patience=5, verbose=1, mode='max'))
+    #     train_callbacks.append(callbacks.EarlyStopping(monitor='val_accuracy', patience=12, restore_best_weights=True, verbose=1, mode='max'))
+
+    return train_callbacks
+
+
+def log_best_cell_results_during_search(logger: logging.Logger, cell_spec: list, best_score: float, metric: str, index: int = 0):
+    logger.info('%s', f' BEST CELL INFO ({index}) ')
+    logger.info('Cell specification:')
+    for i, block in enumerate(cell_spec):
+        logger.info("\tBlock %d: %s", i + 1, rstr(block))
+    logger.info('Best score (%s) reached during training: %0.4f', metric, best_score)
+
+
+def create_model_log_folder(log_path: str):
+    create_empty_folder(log_path)
+    os.mkdir(os.path.join(log_path, 'weights'))  # create weights folder
+    os.mkdir(os.path.join(log_path, 'tensorboard'))  # create tensorboard folder
+
+    log_service.set_log_path(log_path)
+
+
+def save_trimmed_json_config(config: dict, save_path: str):
+    # remove useless keys (config is a subset of search algorithm config)
+    keep_keys = ['cnn_hp', 'architecture_parameters', 'dataset', 'search_strategy']
+    deletable_keys = [k for k in config.keys() if k not in keep_keys]
+
+    for key in deletable_keys:
+        del config[key]
+
+    with open(os.path.join(save_path, 'run.json'), 'w') as f:
+        json.dump(config, f, indent=4)
+
+
+def log_final_training_results(logger: logging.Logger, hist: callbacks.History, score_metric: str, training_time: float, using_multi_output: bool):
+    # hist.history is a dictionary of lists (each metric is a key)
+    if using_multi_output:
+        epoch_index, best_val_score, epoch_metrics_per_output = get_multi_output_best_epoch_stats(hist, score_metric)
+    else:
+        epoch_index, best_val_score = max(enumerate(hist.history[f'val_{score_metric}']), key=operator.itemgetter(1))
+        epoch_metrics_per_output = {'best': {}}
+        epoch_metrics_per_output['best']['val_loss'] = hist.history[f'val_loss'][epoch_index]
+        epoch_metrics_per_output['best']['val_acc'] = hist.history[f'val_accuracy'][epoch_index]
+        epoch_metrics_per_output['best']['val_top_k'] = hist.history[f'val_top_k_categorical_accuracy'][epoch_index]
+        epoch_metrics_per_output['best']['val_f1'] = hist.history[f'val_f1_score'][epoch_index]
+
+        epoch_metrics_per_output['best']['loss'] = hist.history[f'loss'][epoch_index]
+        epoch_metrics_per_output['best']['acc'] = hist.history[f'accuracy'][epoch_index]
+        epoch_metrics_per_output['best']['top_k'] = hist.history[f'top_k_categorical_accuracy'][epoch_index]
+        epoch_metrics_per_output['best']['f1'] = hist.history[f'f1_score'][epoch_index]
+
+    logger.info('*' * 31 + ' TRAINING SUMMARY ' + '*' * 31)
+    logger.info('Best epoch index: %d', epoch_index + 1)
+    logger.info('Total epochs: %d', len(hist.epoch))  # should work also for early stopping
+    logger.info('Best validation %s: %0.4f', score_metric, best_val_score)
+    logger.info('Total training time (without callbacks): %0.4f seconds (%d hours %d minutes %d seconds)',
+                training_time, training_time // 3600, (training_time // 60) % 60, training_time % 60)
+
+    for key in epoch_metrics_per_output:
+        log_title = ' BEST EPOCH ' if key == 'best' else f' Cell {key} '
+        logger.info('*' * 36 + log_title + '*' * 36)
+
+        logger.info('Validation')
+        logger.info('\tValidation accuracy: %0.4f', epoch_metrics_per_output[key]['val_acc'])
+        logger.info('\tValidation loss: %0.4f', epoch_metrics_per_output[key]['val_loss'])
+        logger.info('\tValidation top_k accuracy: %0.4f', epoch_metrics_per_output[key]['val_top_k'])
+        logger.info('\tValidation average f1 score: %0.4f', epoch_metrics_per_output[key]['val_f1'])
+
+        logger.info('Training')
+        logger.info('\tAccuracy: %0.4f', epoch_metrics_per_output[key]['acc'])
+        logger.info('\tLoss: %0.4f', epoch_metrics_per_output[key]['loss'])
+        logger.info('\tTop_k accuracy: %0.4f', epoch_metrics_per_output[key]['top_k'])
+        logger.info('\tAverage f1 score: %0.4f', epoch_metrics_per_output[key]['f1'])
+
+    logger.info('*' * 80)
