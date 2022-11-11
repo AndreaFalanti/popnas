55d82bd473c4eeedd903f1657f63e48fe2258033
AndreaFalanti <a.falanti@gmail.com>
2022-11-11 13:20:14 +0100

    Add new entry point for running E2E the search and post-search scripts
    run_e2e.py combines the run.py code and call the plot slideshow, model selection and last training scripts in sequence, setting the parameters based on the results of previous scripts. This new executable file automatize the entire POPNAS pipeline and can be very valuable to optimize the execution time while running the new experiments. It's still possible to execute each script separately, giving more freedom during development.

--- src/run_e2e.py
+++ src/run_e2e.py
@@ -3,7 +3,10 @@ import json
 import os.path
 import sys
 
+import pandas as pd
+
 import log_service
+import scripts
 from popnas import Popnas
 from utils.config_validator import validate_config_json
 from utils.nn_utils import initialize_train_strategy
@@ -12,8 +15,13 @@ from utils.nn_utils import initialize_train_strategy
 def main():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-r', metavar='RESTORE_FOLDER', type=str, help='path of log folder to restore', default=None)
-    parser.add_argument('-j', metavar='JSON_PATH', type=str, help='path to config json with run parameters', default=None)
-    parser.add_argument('--name', metavar='RUN_NAME', type=str, help='name used for log folder', default=None)
+    parser.add_argument('-j', metavar='JSON_PATH', type=str, help='path to config json for NAS', default=None)
+    parser.add_argument('-jms', metavar='JSON_PATH_MODEL_SEARCH', type=str, help='path to config json for model search', default=None)
+    parser.add_argument('-jlt', metavar='JSON_PATH', type=str, help='path to config json for last training', default=None)
+    parser.add_argument('-params', metavar='PARAMS RANGE', type=str,
+                        help="desired params range for model selection, semicolon separated (e.g. 2.5e6;3.5e6)", default=None)
+    parser.add_argument('-b', metavar='POST_SEARCH_BATCH_SIZE', type=int, help='batch size used in post search scripts', default=None)
+    parser.add_argument('-name', metavar='RUN_NAME', type=str, help='name used for log folder', default=None)
     args = parser.parse_args()
 
     # create folder structure for log files or reuse previous logs to continue execution
@@ -46,6 +54,20 @@ def main():
 
     popnas = Popnas(run_config, train_strategy)
     popnas.start()
+
+    run_folder_path = log_service.log_path
+    post_batch_size = run_config['dataset']['batch_size'] if args.b is None else args.b
+    scripts.generate_plot_slides(run_folder_path, save=True)
+    scripts.execute_model_selection_training(run_folder_path, b=post_batch_size, params=args.params, j=args.jms)
+
+    # get model selection best result
+    model_selection_results_csv_path = os.path.join(run_folder_path, 'best_model_training_top5', 'training_results.csv')
+    ms_df = pd.read_csv(model_selection_results_csv_path)
+    best_ms = ms_df[ms_df['val_score'] == ms_df['val_score'].max()].to_dict('records')[0]
+    print(f'Best model found: {best_ms}')
+
+    scripts.execute_last_training(run_folder_path, b=post_batch_size, f=best_ms['f'], m=best_ms['m'], n=best_ms['n'], spec=best_ms['cell_spec'],
+                                  j=args.jlt)
 
 
 if __name__ == '__main__':
