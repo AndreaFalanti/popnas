40a268e8f8f3d86884db5a27cc1e2b47cc6e5b8b
AndreaFalanti <a.falanti@gmail.com>
2022-11-17 16:30:31 +0100

    Refactor E2E TSC to execute each step in a separate process
    Tensorflow seems to have severe memory leaks when using multiple datasets and multiple network to train. Using processes allow to "destroy" the leaks when the process is terminated. The cost to allocate processes is minor, totally negligible compared to the other operations.

--- src/run_e2e_tsc_archives.py
+++ src/run_e2e_tsc_archives.py
@@ -3,6 +3,7 @@ import json
 import os.path
 import pickle
 import sys
+from multiprocessing import Process
 from typing import Any
 
 import pandas as pd
@@ -69,43 +70,68 @@ def main():
         base_config = json.load(f)
 
     for ds_meta in datasets_metadata:
-        # create folder structure for log files or reuse a previous log folder to continue a stopped/crashed execution
+        # create folder structure for log files
+        root_folder = 'multivariate' if ds_meta.multivariate else 'univariate'
+        run_folder_path = os.path.join(args.name, root_folder, ds_meta.name)
         try:
-            root_folder = 'multivariate' if ds_meta.multivariate else 'univariate'
-            log_service.initialize_log_folders(os.path.join(args.name, root_folder, ds_meta.name))
+            log_service.initialize_log_folders(run_folder_path)
         except AttributeError:
             print(f'Skipping dataset "{ds_meta.name}" since its folder already exists...')
             continue
 
+        log_path = os.path.join('logs', run_folder_path)
         customized_config = adapt_config_to_dataset_metadata(base_config, ds_meta)
 
-        # copy config for possible run restore and post-search scripts
-        with open(log_service.build_path('restore', 'run.json'), 'w') as f:
-            json.dump(customized_config, f, indent=4)
+        # NOTE: run each step of the E2E experiment in a separate process, to "encapsulate and destroy" memory leaks caused by Tensorflow...
+        # processes are run sequentially (immediate join). The workflow is exactly the same of just calling the functions.
+        process = Process(target=run_search_experiment, args=(log_path, customized_config))
+        process.start()
+        process.join()
 
-        # Handle uncaught exception in a special log file, leave it before validating JSON so the exception is logged
-        sys.excepthook = log_service.make_exception_handler(log_service.create_critical_logger())
-        # check that the config is correct
-        validate_config_json(customized_config)
-
-        train_strategy = initialize_train_strategy(customized_config['others']['train_strategy'])
-
-        popnas = Popnas(customized_config, train_strategy)
-        popnas.start()
-
-        run_folder_path = log_service.log_path
         post_batch_size = customized_config['dataset']['batch_size'] if args.b is None else args.b
-        scripts.generate_plot_slides(run_folder_path, save=True)
-        scripts.execute_model_selection_training(run_folder_path, b=post_batch_size, params=args.params, j=args.jms)
 
-        # get model selection best result
-        model_selection_results_csv_path = os.path.join(run_folder_path, 'best_model_training_top5', 'training_results.csv')
-        ms_df = pd.read_csv(model_selection_results_csv_path)
-        best_ms = ms_df[ms_df['val_score'] == ms_df['val_score'].max()].to_dict('records')[0]
-        print(f'Best model found during model selection: {best_ms}')
+        process = Process(target=run_model_selection, args=(log_path, post_batch_size, args.params, args.jms))
+        process.start()
+        process.join()
 
-        scripts.execute_last_training(run_folder_path, b=post_batch_size, f=best_ms['f'], m=best_ms['m'], n=best_ms['n'], spec=best_ms['cell_spec'],
-                                      j=args.jlt)
+        process = Process(target=run_last_training, args=(log_path, post_batch_size, args.jlt))
+        process.start()
+        process.join()
+
+
+def run_search_experiment(log_path: str, customized_config: dict):
+    log_service.set_log_path(log_path)
+
+    # copy config for possible run restore and post-search scripts
+    with open(log_service.build_path('restore', 'run.json'), 'w') as f:
+        json.dump(customized_config, f, indent=4)
+
+    # Handle uncaught exception in a special log file, leave it before validating JSON so the exception is logged
+    sys.excepthook = log_service.make_exception_handler(log_service.create_critical_logger())
+    # check that the config is correct
+    validate_config_json(customized_config)
+
+    train_strategy = initialize_train_strategy(customized_config['others']['train_strategy'])
+
+    popnas = Popnas(customized_config, train_strategy)
+    popnas.start()
+
+    scripts.generate_plot_slides(log_path, save=True)
+
+
+def run_model_selection(run_folder_path: str, batch_size: int, params_range: str, json_path: str):
+    scripts.execute_model_selection_training(run_folder_path, b=batch_size, params=params_range, j=json_path)
+
+
+def run_last_training(run_folder_path: str, batch_size: int, json_path: str):
+    # get model selection best result
+    model_selection_results_csv_path = os.path.join(run_folder_path, 'best_model_training_top5', 'training_results.csv')
+    ms_df = pd.read_csv(model_selection_results_csv_path)
+    best_ms = ms_df[ms_df['val_score'] == ms_df['val_score'].max()].to_dict('records')[0]
+    print(f'Best model found during model selection: {best_ms}')
+
+    scripts.execute_last_training(run_folder_path, b=batch_size, f=best_ms['f'], m=best_ms['m'], n=best_ms['n'], spec=best_ms['cell_spec'],
+                                  j=json_path)
 
 
 if __name__ == '__main__':
