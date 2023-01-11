79ce71fac940896ff52c722420c61f2194026f83
AndreaFalanti <a.falanti@gmail.com>
2023-01-11 17:42:11 +0100

    Add parameter for specifying K of model selection in E2E entrypoint

--- src/run_e2e.py
+++ src/run_e2e.py
@@ -17,6 +17,7 @@ def main():
     parser.add_argument('-j', metavar='JSON_PATH', type=str, help='path to config json for NAS', default=None)
     parser.add_argument('-jms', metavar='JSON_PATH_MODEL_SEARCH', type=str, help='path to config json for model search', default=None)
     parser.add_argument('-jlt', metavar='JSON_PATH', type=str, help='path to config json for last training', default=None)
+    parser.add_argument('-k', metavar='NUM_TOP_MODELS', type=int, help='number of top models to consider in model selection', default=5)
     parser.add_argument('-params', metavar='PARAMS RANGE', type=str,
                         help="desired params range for model selection, semicolon separated (e.g. 2.5e6;3.5e6)", default=None)
     parser.add_argument('-b', metavar='POST_SEARCH_BATCH_SIZE', type=int, help='batch size used in post search scripts', default=None)
@@ -25,7 +26,7 @@ def main():
 
     # create folder structure for log files or reuse a previous log folder to continue a stopped/crashed execution
     run_config = initialize_search_config_and_logs(args.name, args.j, args.r)
-    # Handle uncaught exception in a special log file, leave it before validating JSON so the exception is logged
+    # Handle uncaught exception in a special log file. Leave this before validating JSON, so its eventual exception is logged
     sys.excepthook = log_service.make_exception_handler(log_service.create_critical_logger())
     # check that the config is correct
     validate_config_json(run_config)
@@ -38,7 +39,7 @@ def main():
     run_folder_path = log_service.log_path
     post_batch_size = run_config['dataset']['batch_size'] if args.b is None else args.b
     scripts.generate_plot_slides(run_folder_path, save=True)
-    scripts.execute_model_selection_training(run_folder_path, b=post_batch_size, params=args.params, j=args.jms)
+    scripts.execute_model_selection_training(run_folder_path, b=post_batch_size, params=args.params, j=args.jms, k=args.k)
 
     # get model selection best result
     model_selection_results_csv_path = os.path.join(run_folder_path, 'best_model_training_top5', 'training_results.csv')
