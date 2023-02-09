942e1c5fdb1715a6e3d496da121d168ed3bfab44
AndreaFalanti <a.falanti@gmail.com>
2023-02-09 15:45:12 +0100

    Disable graphs save from tensorboard callback
    TF graphs are difficult to read and very low level compared to the already available Keras ones, and require quite a bit of space. This change saves a few hundreds of MBs :)

--- src/models/generators/base.py
+++ src/models/generators/base.py
@@ -539,7 +539,7 @@ class BaseModelGenerator(ABC):
             Keras callbacks to apply in model.fit() function.
         '''
         # By default, shows losses and metrics for both training and validation
-        model_callbacks = [callbacks.TensorBoard(log_dir=tb_logdir, profile_batch=0, histogram_freq=0, update_freq='epoch')]
+        model_callbacks = [callbacks.TensorBoard(log_dir=tb_logdir, profile_batch=0, histogram_freq=0, update_freq='epoch', write_graph=False)]
 
         if self.save_weights:
             # Save best weights, using as metric the last output in case of multi-output models
@@ -547,7 +547,7 @@ class BaseModelGenerator(ABC):
             last_used_cell = max(self.network_build_info.used_cell_indexes)
             target_metric = f'val_{output_layer_name}_c{last_used_cell}_{score_metric}' \
                 if self.multi_output and self.network_build_info.blocks > 0 else f'val_{score_metric}'
-            model_callbacks.append(callbacks.ModelCheckpoint(filepath=os.path.join(tb_logdir, 'best_weights.h5'),
+            model_callbacks.append(callbacks.ModelCheckpoint(filepath=os.path.join(tb_logdir, 'best_weights.ckpt'),
                                                              save_weights_only=True, save_best_only=True, monitor=target_metric, mode='max'))
 
         # TODO: if you want to use early stopping, training time should be rescaled for predictor
