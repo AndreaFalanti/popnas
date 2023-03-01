0e5057b871c6051690bbd7ffa8e2957668392871
AndreaFalanti <a.falanti@gmail.com>
2023-03-01 18:39:52 +0100

    Fix bug in TSC preprocessing model
    It was actually not present in TF 2.7, fortunately it was an easy fix...

--- src/dataset/preprocessing/time_series_preprocessor.py
+++ src/dataset/preprocessing/time_series_preprocessor.py
@@ -25,17 +25,16 @@ class TimeSeriesPreprocessor(DataPreprocessor):
         return ds
 
     def _build_preprocessing_embeddable_model(self, ds: tf.data.Dataset) -> Optional[Sequential]:
-        preprocessing_keras_layers = []
+        # by specifying the input, the last dimension is preserved even in time series with a single features
+        # without Input, Keras will butcher the last dimension just to cause a nice shape incompatibility with the next layers :)
+        preprocessing_keras_layers = [layers.Input(shape=(None, None))]
 
         if self.normalize:
-            # last axis has the features of each time step, we want to normalize each feature independently
+            # the last axis has the features of each time step, we want to normalize each feature independently
             norm = layers.Normalization(axis=-1)
-            # tested with debugger, computes the average and mean on all dataset correctly in this way
+            # tested with debugger, it correctly computes the average and mean on all dataset samples in this way
             norm.adapt(ds.map(lambda x, y: x, num_parallel_calls=AUTOTUNE))
             preprocessing_keras_layers.append(norm)
 
         # if at least a model-embedded layer exists, generate the Keras preprocessing model, otherwise return None
-        return Sequential(layers=preprocessing_keras_layers, name='preprocessing') if len(preprocessing_keras_layers) > 0 \
-            else None
-
-
+        return Sequential(layers=preprocessing_keras_layers, name='preprocessing') if len(preprocessing_keras_layers) > 0 else None
