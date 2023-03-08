9c003fe0d4efcf465f807ef50462fc1d020bcc90
AndreaFalanti <a.falanti@gmail.com>
2023-03-08 21:26:34 +0100

    Change ModelGenerator used in segmentation tasks

--- src/models/generators/factory.py
+++ src/models/generators/factory.py
@@ -14,8 +14,8 @@ def model_generator_factory(task: str, cnn_hp: CnnHpConfig, arc_params: Architec
         return ClassificationModelGenerator(cnn_hp, arc_params, training_steps_per_epoch, output_classes_count, input_shape,
                                             data_augmentation_model, preprocessing_model, save_weights)
     elif task == 'image_segmentation':
-        return SegmentationModelGenerator(cnn_hp, arc_params, training_steps_per_epoch, output_classes_count, input_shape,
-                                          data_augmentation_model, preprocessing_model, save_weights)
+        return SegmentationFixedDecoderModelGenerator(cnn_hp, arc_params, training_steps_per_epoch, output_classes_count, input_shape,
+                                                      data_augmentation_model, preprocessing_model, save_weights)
     else:
         raise ValueError('Dataset task type is not supported by POPNAS or invalid')
 
@@ -28,6 +28,6 @@ def get_model_generator_class_for_task(task: str) -> Type[BaseModelGenerator]:
     if task == 'image_classification' or task == 'time_series_classification':
         return ClassificationModelGenerator
     elif task == 'image_segmentation':
-        return SegmentationModelGenerator
+        return SegmentationFixedDecoderModelGenerator
     else:
         raise ValueError('Dataset task type is not supported by POPNAS or invalid')
