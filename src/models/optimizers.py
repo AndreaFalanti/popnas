fb2c451b8795852a4548b86b2676830d21d7baf5
AndreaFalanti <a.falanti@gmail.com>
2023-03-10 02:10:51 +0100

    Add new module for generating scheduler and optimizers from regexes
    Will be used in a new configuration file format, coming soon :) Add also tests about the new module.

---
+++ src/models/optimizers.py
@@ -0,0 +1,92 @@
+import re
+from typing import Union, TypeVar
+
+from tensorflow.keras import optimizers
+from tensorflow.keras.optimizers import schedules
+from tensorflow_addons import optimizers as tfa_optimizers
+
+from utils.config_dataclasses import OptimizerDict
+
+decimal_regex = r'\d+.?\d*'
+
+T = TypeVar('T', int, str, float)
+Rate = Union[float, schedules.LearningRateSchedule]
+
+
+def get_group_or_default(match: re.Match, group_id: Union[int, str], default: T) -> T:
+    group_val = match.group(group_id)
+    # tricky code to convert "group_val" to the same type of the default value provided
+    return default if group_val is None else type(default)(group_val)
+
+
+def build_scheduler(scheduler_str: str, learning_rate: float, weight_decay: float, training_steps_per_epoch: int, epochs: int) -> 'tuple[Rate, Rate]':
+    # fixed rates (no schedules)
+    if scheduler_str is None or scheduler_str == '':
+        return learning_rate, weight_decay
+
+    # Cosine Decay Restart
+    match = re.compile(
+        rf'cdr:?(?: (?P<t_mul>{decimal_regex}) t_mul)?,?(?: (?P<m_mul>{decimal_regex}) m_mul)?,?(?: (?P<alpha>{decimal_regex}) alpha)?') \
+        .match(scheduler_str)
+    if match:
+        t_mul = get_group_or_default(match, 't_mul', 2.0)
+        m_mul = get_group_or_default(match, 'm_mul', 1.0)
+        alpha = get_group_or_default(match, 'alpha', 0.0)
+        decay_steps = training_steps_per_epoch * t_mul
+
+        return schedules.CosineDecayRestarts(learning_rate, decay_steps, t_mul=t_mul, m_mul=m_mul, alpha=alpha), \
+            schedules.CosineDecayRestarts(weight_decay, decay_steps, t_mul=t_mul, m_mul=m_mul, alpha=alpha)
+
+    # Cosine Decay
+    match = re.compile(rf'cd:?(?: (?P<alpha>{decimal_regex}) alpha)?').match(scheduler_str)
+    if match:
+        total_training_steps = training_steps_per_epoch * epochs
+        alpha = get_group_or_default(match, 'alpha', 0.0)
+
+        return schedules.CosineDecay(learning_rate, total_training_steps, alpha=alpha), \
+            schedules.CosineDecay(weight_decay, total_training_steps, alpha=alpha)
+
+    raise AttributeError(f'Scheduler "{scheduler_str}" not recognized')
+
+
+def build_optimizer(optimizer_str: str, learning_rate: Rate, weight_decay: Rate, total_training_steps: int) -> optimizers.Optimizer:
+    # AdamW
+    match = re.compile(r'adamW').match(optimizer_str)
+    if match:
+        return tfa_optimizers.AdamW(weight_decay, learning_rate)
+
+    # Adam
+    match = re.compile(r'adam').match(optimizer_str)
+    if match:
+        return optimizers.Adam(learning_rate)
+
+    # SGDW
+    match = re.compile(rf'SGDW:?(?: (?P<momentum>{decimal_regex}) momentum)?').match(optimizer_str)
+    if match:
+        momentum = get_group_or_default(match, 'momentum', 0.0)
+        return tfa_optimizers.SGDW(weight_decay, learning_rate, momentum)
+
+    # SGD
+    match = re.compile(rf'SGD:?(?: (?P<momentum>{decimal_regex}) momentum)?').match(optimizer_str)
+    if match:
+        momentum = get_group_or_default(match, 'momentum', 0.0)
+        return optimizers.SGD(learning_rate, momentum)
+
+    # Radam
+    match = re.compile(rf'radam:?(?: (?P<alpha>{decimal_regex}) alpha)?').match(optimizer_str)
+    if match:
+        alpha = get_group_or_default(match, 'alpha', 0.0)
+        return tfa_optimizers.RectifiedAdam(learning_rate, weight_decay=weight_decay, total_steps=total_training_steps,
+                                            min_lr=learning_rate * alpha, warmup_proportion=0.1)
+
+
+def instantiate_optimizer_and_schedulers(optimizer_config: OptimizerDict, learning_rate: float, weight_decay: float,
+                                         training_steps_per_epoch: int, epochs: int) -> optimizers.Optimizer:
+    lr, wd = build_scheduler(optimizer_config.scheduler, learning_rate, weight_decay, training_steps_per_epoch, epochs)
+    optimizer = build_optimizer(optimizer_config.type, lr, wd, training_steps_per_epoch * epochs)
+
+    lookahead = optimizer_config.lookahead
+    if lookahead is None:
+        return optimizer
+    else:
+        return tfa_optimizers.Lookahead(optimizer, lookahead.sync_period, lookahead.slow_step_size)
---
+++ tests/test_optimizer_instantiator.py
@@ -0,0 +1,75 @@
+import unittest
+
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.optimizers.schedules import *
+from tensorflow_addons.optimizers import *
+
+from models.optimizers import build_optimizer, build_scheduler
+
+# some random parameters
+learning_rate = 0.1
+weight_decay = 0.1
+training_steps_per_epoch = 1000
+epochs = 10
+
+
+class TestOptimizerInstantiator(unittest.TestCase):
+    def test_scheduler_builder(self):
+        lr_scheduler, wd_scheduler = build_scheduler('cd', learning_rate, weight_decay, training_steps_per_epoch, epochs)
+        self.assertIsInstance(lr_scheduler, CosineDecay)
+        self.assertIsInstance(wd_scheduler, CosineDecay)
+        # default value of alpha
+        self.assertEqual(lr_scheduler.alpha, 0.0)
+        self.assertEqual(wd_scheduler.alpha, 0.0)
+
+        lr_scheduler, wd_scheduler = build_scheduler('cd: 0.1 alpha', learning_rate, weight_decay, training_steps_per_epoch, epochs)
+        self.assertEqual(lr_scheduler.alpha, 0.1)
+        self.assertEqual(wd_scheduler.alpha, 0.1)
+
+        lr_scheduler, wd_scheduler = build_scheduler('cdr', learning_rate, weight_decay, training_steps_per_epoch, epochs)
+        self.assertIsInstance(lr_scheduler, CosineDecayRestarts)
+        self.assertIsInstance(wd_scheduler, CosineDecayRestarts)
+        # t_mul and m_mul are protected, hopefully they will not change the name
+        self.assertEqual(lr_scheduler.alpha, 0.0)
+        self.assertEqual(lr_scheduler._t_mul, 2.0)
+        self.assertEqual(lr_scheduler._m_mul, 1.0)
+        self.assertEqual(wd_scheduler.alpha, 0.0)
+        self.assertEqual(wd_scheduler._t_mul, 2.0)
+        self.assertEqual(wd_scheduler._m_mul, 1.0)
+
+        lr_scheduler, wd_scheduler = build_scheduler('cdr: 3 t_mul, 0.9 m_mul, 0.1 alpha', learning_rate, weight_decay, training_steps_per_epoch, epochs)
+        self.assertIsInstance(lr_scheduler, CosineDecayRestarts)
+        self.assertIsInstance(wd_scheduler, CosineDecayRestarts)
+        self.assertEqual(lr_scheduler.alpha, 0.1)
+        self.assertEqual(lr_scheduler._t_mul, 3.0)
+        self.assertEqual(lr_scheduler._m_mul, 0.9)
+        self.assertEqual(wd_scheduler.alpha, 0.1)
+        self.assertEqual(wd_scheduler._t_mul, 3.0)
+        self.assertEqual(wd_scheduler._m_mul, 0.9)
+
+    def test_optimizer_builder(self):
+        total_training_steps = training_steps_per_epoch * epochs
+
+        optimizer = build_optimizer('adam', learning_rate, weight_decay, total_training_steps)
+        self.assertIsInstance(optimizer, Adam)
+
+        optimizer = build_optimizer('adamW', learning_rate, weight_decay, total_training_steps)
+        self.assertIsInstance(optimizer, AdamW)
+
+        optimizer = build_optimizer('SGD', learning_rate, weight_decay, total_training_steps)
+        self.assertIsInstance(optimizer, SGD)
+        # self.assertEqual(optimizer.momentum.numpy(), 0.0)
+
+        optimizer = build_optimizer('SGD: 0.9 momentum', learning_rate, weight_decay, total_training_steps)
+        self.assertIsInstance(optimizer, SGD)
+        # self.assertEqual(optimizer.momentum.numpy(), np.float(0.9)) # Not working, 0.9 != 0.9 (?)
+
+        optimizer = build_optimizer('SGDW', learning_rate, weight_decay, total_training_steps)
+        self.assertIsInstance(optimizer, SGDW)
+
+        optimizer = build_optimizer('radam', learning_rate, weight_decay, total_training_steps)
+        self.assertIsInstance(optimizer, RectifiedAdam)
+
+
+if __name__ == '__main__':
+    unittest.main()
